2b60ff1a206c731feda36c540a2b574d35787410
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-20 14:29:01 +0300

    .Net Getting and Showing data using Model

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Books()
+        {
+            Books b = new Books();
+            b.serialno = 1432;
+            b.booksname = "Çalıkuşu";
+            b.authorsname = "Reşat Nuri";
+            b.age = 22;
+            return View(b);
+        }
+        [HttpPost]
+        public ActionResult Books(Books model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ MVC/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Books
+    {
+        public int serialno { get; set; }
+        public string booksname { get; set; }
+        public string authorsname { get; set; }
+        public int age { get; set; }
+    }
+}
